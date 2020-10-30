b553b01723a70c9117474a2c4b9f46931ca21261
Tabatha Bell <tabathambell@live.com>
2020-10-30 16:56:12 -0400

    added generator skeleton

--- Develop/script.js
+++ Develop/script.js
@@ -6,12 +6,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?<>:{}[]";
+  var passwordLength = 20;
+  var password = writePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+  for (var i=0; i<passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber,randomNumber+1);
+  }
+    document.getElementById("password").value = password
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+element.addEventListener("click", writePassword);
