ef87873ac378f387abc49ab917ef2cec42bbbbe7
Tabatha Bell <tabathambell@live.com>
2020-10-30 21:35:21 -0400

    confirmations and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -5,16 +5,38 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// used different variable names to help myself remember
 function writePassword() {
   var smolChars = "abcdefghijklmnopqrstuvwxyz";
-  var bigchungusChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var mahdigitsChars = "0123456789"
-  var specialChars = "!@#$%^&*,.()_+?<>:{}[]"
-  var passwordLength = 20;
+  var bigchungusChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var mahdigitsChars = "0123456789";
+  var specialChars = "!@#$%^&*,.()_+?<>:{}[]";
+  
+  var passwordLength = window.prompt("Please enter a password length.");
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a valid number between 8 and 128.");
+    return;
+  }
+
+  var confirmSmol = confirm("Do you want lowercase characters in your password?");
+  var confirmBigchungus = confirm("Do you want uppercase characters in your password?");
+  var confirmMahdigits = confirm("Do you want digits in your password?");
+  var confirmSpecial = confirm("Do you want special characters in your password?");
+
+  if (!confirmSmol && !confirmBigchungus && !confirmMahdigits && !confirmSpecial) {
+    alert("You have to have SOMETHING in your password!");
+    return;
+  }
+
   var password = "";
   var passwordText = document.querySelector("#password");
 
-  for (var i=0; i<passwordLength; i++){
+  for (var i = 0; i < passwordLength; i++) {
+    var chars = "";
+    if (confirmSmol) {
+      chars += smolChars;
+    }
+
     var randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber,randomNumber+1);
   }
@@ -22,4 +44,4 @@ function writePassword() {
 };
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("onMouseUp", writePassword);
